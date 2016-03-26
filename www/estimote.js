f983c90f42e29a0e060435bcdc916ce58877922e
Brendan Ingham <brendan@the-tool-shed.uk>
2016-03-26 11:12:07 +0000

    Re-Adding the default StartRanging and moving the newer Function to scanOnce

--- www/estimote.js
+++ www/estimote.js
@@ -1,42 +1,67 @@
 var cordova = require('cordova');
 var exec = require('cordova/exec');
 
-function Estimote() {}
+function Estimote() {};
 
-Estimote.prototype.startRanging = function startRanging(region) {
-    
+Estimote.prototype.scanOnce = function scanOnce(region) {
+
     var options = {};
-    
+
     if(typeof region === 'object') {
       options = region;
     } else if ( typeof region === 'string' ) {
       options.region = region;
-    } 
-    
+    }
+
     return new Promise( function(resolve, reject) {
       /**
-       * all the usability of _notification + more. 
+       * all the usability of _notification + more.
        **/
       var resolve_function = function(info) {
         if( info.beacons.length > 0) {
           cordova.fireDocumentEvent('beaconsReceived', info);
+          this.stopRanging();
           resolve(info);
         }
-      }
-      /** 
-       *  Actually make the error useful! 
+      }.bind(this);
+      /**
+       *  Actually make the error useful!
        **/
       var reject_function = function(error) {
         console.error(error);
+        this.stopRanging();
         reject(error);
-      }
-      
+      }.bind(this);
+
       exec(resolve_function, reject_function, 'Estimote', 'startRanging', [options] );
-      
+
     });
 };
 
-Estimote.prototype.stopRanging = function() {
+Estimote.prototype.startRanging = function startRanging(region) {
+  var options = {};
+
+  if(typeof region === 'object') {
+    options = region;
+  } else if ( typeof region === 'string' ) {
+    options.region = region;
+  }
+
+  var resolve_function = function(info) {
+    if( info.beacons.length > 0) {
+      cordova.fireDocumentEvent('beaconsReceived', info);
+    }
+  }
+
+  var reject_function = function(error) {
+    console.error(error);
+  }
+
+  exec(resolve_function, reject_function, 'Estimote', 'startRanging', [options] );
+
+}
+
+Estimote.prototype.stopRanging = function stopRanging() {
   exec(null, null, 'Estimote', 'stopRanging', []);
 };
 
